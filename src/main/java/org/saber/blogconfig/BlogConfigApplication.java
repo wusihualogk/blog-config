1de7ecc4253e450064fbee7c91dad0cf383f2089
saber <707639014@qq.com>
2019-05-03 13:43:43 +0800

    init

---
+++ src/main/java/org/saber/blogconfig/BlogConfigApplication.java
@@ -0,0 +1,17 @@
+package org.saber.blogconfig;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@SpringBootApplication
+@EnableConfigServer
+@EnableEurekaClient
+public class BlogConfigApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BlogConfigApplication.class, args);
+    }
+
+}
